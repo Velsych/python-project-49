bbcec922c540650e747c3f648678a68d43f30291
Velsych <velsych@mail.ru>
2025-05-06 10:12:00 +0300

    Modified init and added new script line

---
+++ brain_games/__init__.py
@@ -0,0 +1,6 @@
+from brain_games.module import user_greet
+from brain_games.cli import welcome_user
+
+__all__ = (
+    "user_greet","welcome_user"
+)   
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,8 @@
-from brain_games.module import user_greet
+from brain_games import user_greet, welcome_user
 
 def main():
     user_greet()
+    welcome_user()
 
 if __name__ == "__main__":
     user_greet()
