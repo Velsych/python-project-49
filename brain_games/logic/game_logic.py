5bc656f59bdbeaf73b889299544ad2969443f2fd
Velsych <velsych@mail.ru>
2025-05-09 12:48:27 +0300

    fixes in games_logic

--- brain_games/logic/game_logic.py
+++ brain_games/logic/game_logic.py
@@ -16,5 +16,5 @@ def congrats(user_name):
 def hello_username():
     import prompt
     name = prompt.string("May I have your name? ")
-    print(f'Hello {name}!')
+    print(f'Hello, {name}!')
     return name
